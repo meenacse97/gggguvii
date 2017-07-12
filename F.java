f101688a1d23395f44ed98432a016d687b672320
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-12 16:17:27 +0530

    Add files via upload

---
+++ F.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.*;
+public class F {
+	public static void main(String[] args) {
+		String a[]={"AF","D","EF","G"};
+		for(int i=0;i<a.length;i++)
+		{
+            if(a[i].length()==1)		
+			{
+				System.out.println(a[i]);
+			
+			}
+		}
+
+	}
+
+
+}
