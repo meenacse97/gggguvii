f453180f707a69324254e3a647434e49247399c8
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-12 16:05:47 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,26 @@
+package guvi;
+import java.util.*;
+public class str {
+	public static void main(String []args){
+		int count=0;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number of Strings in the array ");
+		int n=s.nextInt();
+		String str[]=new String[n];
+		System.out.println("Enter the Strings in the array ");
+		for(int i=0;i<n;i++){
+			str[i]=s.next();
+		}
+		for(int i=0;i<n;i++){
+			if(str[i].length()>2){
+				if((str[i].charAt(0)=='0' && str[i].charAt(1)=='1') || (str[i].charAt(0)=='1' && str[i].charAt(1)=='0')){
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+
+}
+
+
